659ac569afe0f89c917180d062eaa203873a5493
Alex <ultv@inbox.ru>
2018-05-21 21:03:25 +0400

    Realized functional.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,22 @@ namespace testTackSeleniumSC
         static void Main(string[] args)
         {
 
-            
+            IWebDriver browser = new ChromeDriver();
+            browser.Navigate().GoToUrl("https://google.com");
+            IWebElement body = browser.FindElement(By.TagName("body"));
+
+            IWebElement SearchInput = browser.FindElement(By.Id("lst-ib"));
+            SearchInput.SendKeys("Simbirsoft" + Keys.Enter);
+
+            IWebElement FirstLink = browser.FindElement(By.ClassName("r"));
+            FirstLink.Click();
+
+            if (browser.Url == "https://www.simbirsoft.com/")
+            {
+                Console.Clear();
+                Console.WriteLine($"\n{browser.Url.ToString()} \n\nпервый в списке поиска по фразе \"Simbirsoft\" на сайте google.com");
+                browser.Close();
+            }
 
         }
     }
