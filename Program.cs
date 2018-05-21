91e04247ca8df205909b5a33cb3853507ff622e5
Alex <ultv@inbox.ru>
2018-05-21 21:12:01 +0400

    Processed the exception of slow loading of elements.

--- Program.cs
+++ Program.cs
@@ -11,18 +11,43 @@ namespace testTackSeleniumSC
 
             IWebDriver browser = new ChromeDriver();
             browser.Navigate().GoToUrl("https://google.com");
-            IWebElement body = browser.FindElement(By.TagName("body"));
 
-            IWebElement SearchInput = browser.FindElement(By.Id("lst-ib"));
-            SearchInput.SendKeys("Simbirsoft" + Keys.Enter);
+            bool ElementIsVissible = false;
+            int HowWait = 15;
 
-            IWebElement FirstLink = browser.FindElement(By.ClassName("r"));
-            FirstLink.Click();
+            while (!ElementIsVissible && HowWait > 0)
+            {
+                try
+                {
+                    IWebElement body = browser.FindElement(By.TagName("body"));
 
-            if (browser.Url == "https://www.simbirsoft.com/")
+                    IWebElement SearchInput = browser.FindElement(By.Id("lst-ib"));
+                    SearchInput.SendKeys("Simbirsoft" + Keys.Enter);
+
+                    IWebElement FirstLink = browser.FindElement(By.ClassName("r"));
+                    FirstLink.Click();
+
+                    if (browser.Url == "https://www.simbirsoft.com/")
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"\n{browser.Url.ToString()} \n\nпервый в списке поиска по фразе \"Simbirsoft\" на сайте google.com");
+                        browser.Close();
+                    }
+
+                    ElementIsVissible = true;
+                }
+                catch
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    ElementIsVissible = false;
+                    HowWait--;
+                }
+            }
+
+            if (!ElementIsVissible)
             {
                 Console.Clear();
-                Console.WriteLine($"\n{browser.Url.ToString()} \n\nпервый в списке поиска по фразе \"Simbirsoft\" на сайте google.com");
+                Console.WriteLine("\nПроблемы с доступом к элементам сайта. Попробуйте повторить позже.");
                 browser.Close();
             }
 
