d54ab24a733413de88c67727f694958ebb0c212f
Alex <ultv@inbox.ru>
2018-05-21 20:56:41 +0400

    Init commit.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace testTackSeleniumSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+        }
+    }
+}
